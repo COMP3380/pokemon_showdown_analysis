61c98f81548845904e72cad45dc8c92f3061bab8
pieberrykinnie <dmtruong1980@gmail.com>
2025-12-01 06:48:24 -0600

    feat: Write Pokemon queries

--- src/setup/process_sql/generate_sql.py
+++ src/setup/process_sql/generate_sql.py
@@ -98,13 +98,13 @@ def generate_metadata(dest: str) -> None:
     INSERT_CUTOFF: str = "INSERT INTO Cutoff (elo) VALUES ({});"
     INSERT_MAPF: str = "INSERT INTO MetagameAllowsPokemonFrom (parentMetagame, childMetagame) VALUES ('{}', '{}');"
 
-    METAGAMES: list[str] = ["Ubers", "OU", "UU", "RU", "NU", "PU", "ZU"]
     PERIODS: list[str] = [
         "('2025-07', '2025-07-01 00:00:00.000', '2025-07-31 23:59:59.997')",
         "('2025-08', '2025-08-01 00:00:00.000', '2025-08-31 23:59:59.997')",
         "('2025-09', '2025-09-01 00:00:00.000', '2025-09-30 23:59:59.997')",
         "('2025-10', '2025-10-01 00:00:00.000', '2025-10-31 23:59:59.997')"
     ]
+    METAGAMES: list[str] = ["Ubers", "OU", "UU", "RU", "NU", "PU", "ZU"]
     CUTOFFS: list[int] = [0, 1760, 1825]
 
     with open(dest, "w") as f:
@@ -125,6 +125,80 @@ def generate_metadata(dest: str) -> None:
     print("Finished writing Metagame, Period, Cutoff, MetagameAllowsPokemonFrom queries into", dest)
 
 
+def generate_pokedex(source: str, dest: str) -> None:
+    # more complicated metagame mapping
+    METAGAMES: dict[str, str] = {
+        "Ubers": "Ubers",
+        "OU": "OU",
+        "UUBL": "OU",
+        "UU": "UU",
+        "RUBL": "UU",
+        "RU": "RU",
+        "NUBL": "RU",
+        "NU": "NU",
+        "PUBL": "NU",
+        "PU": "PU",
+        "ZUBL": "PU",
+        "ZU": "ZU",
+        "NFE": "ZU",
+        "LC": "ZU"
+    }
+    INSERT_POKEMON: str = "INSERT INTO Pokemon VALUES ('{}', '{}', '{}', {}, {}, {}, {}, {}, {}, {}, {});"
+
+    queries: list[str] = []
+
+    with open(source, "r") as f:
+        data: dict = json.load(f)
+
+    pokedex: dict[str, dict[str, str | list[str]
+                            | dict[str, int]]] = data["pokedex"]
+
+    # Insert Pokemon table first
+    for p in pokedex:
+        name: str = pokedex[p]["name"]
+        type1: str = pokedex[p]["types"][0]
+        type2: str
+        if len(pokedex[p]["types"]) > 1:
+            type2 = ("\'" + pokedex[p]["types"][1] + "\'")
+        else:
+            type2 = "NULL"
+        hp: int = pokedex[p]["baseStats"]["hp"]
+        atk: int = pokedex[p]["baseStats"]["atk"]
+        df: int = pokedex[p]["baseStats"]["def"]
+        spa: int = pokedex[p]["baseStats"]["spa"]
+        spd: int = pokedex[p]["baseStats"]["spd"]
+        spe: int = pokedex[p]["baseStats"]["spe"]
+        tier: str
+        if ("tier" in pokedex[p] and pokedex[p]["tier"] in METAGAMES):
+            tier = ("\'" + METAGAMES[pokedex[p]["tier"]] + "\'")
+        else:
+            tier = "NULL"
+
+        queries.append(INSERT_POKEMON.format(p, name, type1,
+                                             type2, hp, atk, df, spa, spd, spe, tier))
+
+    # Insert PokemonHasAbility table
+    for p in pokedex:
+        abilities: list[str] = pokedex[p]["abilities"]
+        for a in abilities:
+            queries.append(
+                f"INSERT INTO PokemonHasAbility (pokemon, ability) VALUES ('{p}', '{a}');")
+
+    # Insert PokemonLearnsMove table
+    for p in pokedex:
+        moves: list[str] = pokedex[p]["moves"]
+        for m in moves:
+            queries.append(
+                f"INSERT INTO PokemonLearnsMove (pokemon, move) VALUES ('{p}', '{m}');")
+
+    with open(dest, "w") as f:
+        for q in queries:
+            # print(q)
+            f.write(q.strip() + "\n")
+
+    print("Finished writing Pokemon, PokemonHasAbility and PokemonLearnsMove queries into", dest)
+
+
 def main() -> None:
     generate_type(
         "../../data/showdown_data_processed/typechart.json", "./type.sql")
@@ -135,6 +209,8 @@ def main() -> None:
     generate_move(
         "../../data/showdown_data_processed/moves.json", "./move.sql")
     generate_metadata("./metadata.sql")
+    generate_pokedex(
+        "../../data/showdown_data_processed/pokedex.json", "./pokedex.sql")
 
 
 if __name__ == "__main__":
