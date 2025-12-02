6ad40450352c3664efc945fa08228ebfabde0096
cjlangan <connorjameslangan@gmail.com>
2025-12-01 20:52:43 -0600

    feat: added Items Page

--- src/app/app.py
+++ src/app/app.py
@@ -1,8 +1,10 @@
+import pymssql
 from textual.app import App
 from screens.menu import Menu
 from screens.moves import Moves
 from screens.pokemon import Pokemon
 from screens.items import Items
+from screens.items_p2 import ItemsP2
 from screens.abilities import Abilities
 from screens.typechart import Typechart
 from screens.stats import Stats
@@ -15,14 +17,31 @@ class PokiTUI(App):
         "moves": Moves,
         "pokemon": Pokemon,
         "items": Items,
+        "items_p2": ItemsP2,
         "abilities": Abilities,
         "typechart": Typechart,
         "stats": Stats,
         "queries": Queries,
     }
 
+    def on_startup(self) -> None:
+        pass
+
     # On startup, show the user the Menu
     def on_mount(self):
+        # Create DB connection 
+        self.conn = pymssql.connect(
+            server="uranium.cs.umanitoba.ca",
+            user="vuqh1",
+            password="7990597",
+            database="cs3380",
+        )
+        self.cursor = self.conn.cursor()
+
+        # Set any variables
+        self.item = ""
+
+        # Start at menu screen
         self.push_screen("menu")
 
 if __name__ == "__main__":
--- src/app/screens/items.py
+++ src/app/screens/items.py
@@ -1,7 +1,8 @@
+from textual import on, events
 from textual.app import ComposeResult
-from textual.widgets import Header, Footer, Button
-from textual.containers import Container
+from textual.widgets import Header, Footer, DataTable, Static
 from textual.screen import Screen
+from .components.filterable_table import FilterableTable
 
 class Items(Screen):
     TITLE = "Items"
@@ -10,12 +11,70 @@ class Items(Screen):
     BINDINGS = [
         ("Ctrl-q", "quit", "Quit"),
         ("m", "menu", "Menu"),
+        ("b", "back", "Back"),
+        ("return", "enter", "Select Highlighted Row"),
     ]
+
+    DEFAULT_CSS = """
+    #msg {
+      margin: 2;
+    }
+    """
 
     def compose(self) -> ComposeResult:
         yield Header()
+        yield Static("Search for your desired Item!", id="msg")
+        yield FilterableTable()
         yield Footer()
+
+    def on_mount(self):
+        self.cursor = getattr(self.app, "cursor") # get the DB connection
+        self.run_query("") # get initial data
+        self.rows = []
 
     def action_menu(self):
         self.app.switch_screen("menu")
 
+    def action_back(self):
+        self.app.pop_screen()
+
+    @on(FilterableTable.FilterChanged)
+    def handle_db_filter(self, message: FilterableTable.FilterChanged):
+        """This captures the custom message from the widget."""
+        self.run_query(message.value)
+
+    def run_query(self, search_term: str):
+        """
+        Connects to DB, gets results, and pushes them into the widget
+        """
+        if not self.cursor:
+            return
+
+        if search_term:
+            sql = "SELECT id, name FROM Item WHERE name LIKE %s"
+            self.cursor.execute(sql, (f"%{search_term}%",))
+        else:
+            sql = "SELECT id, name FROM Item"
+            self.cursor.execute(sql)
+
+        self.rows = self.cursor.fetchall()
+        headers = [desc[0] for desc in self.cursor.description]
+
+        widget = self.query_one(FilterableTable)
+        widget.render_data(headers, self.rows)
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "enter":
+            self.log("Attempted submission")
+
+            # Get the highlighted row in the table
+            table = self.query_one(FilterableTable)
+            data_table = table.query_one(DataTable)
+            row_index = data_table.cursor_row
+
+            # Set the global variable to the selection and change pages
+            if row_index is not None:
+                row = data_table.get_row_at(row_index)
+                setattr(self.app, "item", row[0])
+
+                self.app.push_screen("items_p2")
